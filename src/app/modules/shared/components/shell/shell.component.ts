60ae4266eac675c0a172e5bd3e53ad9e00faecf4
Rahul Barwal <rahulbarwal654@gmail.com>
2021-08-27 22:19:21 +0530

    App shell ready.
    1. removed strict initialization rule from typescript.

--- src/app/modules/shared/components/shell/shell.component.ts
+++ src/app/modules/shared/components/shell/shell.component.ts
@@ -1,5 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
-
+import { map, shareReplay } from "rxjs/operators";
 @Component({
   selector: 'kanban-shell',
   templateUrl: './shell.component.html',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
 
-  constructor() { }
+  isMobileScreen$ = this._bpObserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay());
+  constructor(private _bpObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { ShellComponent } from './components/shell/shell.component';
+
 
 const components = [ShellComponent];
 
@@ -21,7 +21,6 @@ const modules = [
   CommonModule,
   MatButtonModule,
   MatToolbarModule,
-  MatIconModule,
   LayoutModule,
   MatSidenavModule,
   MatListModule,
